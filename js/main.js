407f6b60bb40b0df8c6191bab8db89fe0297d001
gavinbrown025 <you@example.com>
2020-02-14 10:48:27 -0500

    other functions

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(() => {
+
+const playButtons = document.querySelectorAll('.playTrack'),
+	pauseButtons = document.querySelectorAll('.pauseTrack'),
+	rwButtons = document.querySelectorAll('.rwTrack'),
+	audioElement = document.querySelector('#jukebox');
+
+function loadTrack() {
+
+	// what audio src we want to play
+	let audioSource = `audio/${this.dataset.trackref}.mp3`;
+
+	//set the source element of the audio track
+	audioElement.src = audioSource;
+
+	audioElement.load();
+	playTrack();
+}
+
+function playTrack() {
+	audioElement.play();
+}
+
+function pauseTrack() {
+	audioElement.pause();
+}
+
+function rwTrack() {
+	audioElement.currentTime = 0;
+}
+
+playButtons.forEach(button => button.addEventListener('click', loadTrack));
+pauseButtons.forEach(button => button.addEventListener('click', pauseTrack));
+rwButtons.forEach(button => button.addEventListener('click', rwTrack));
+
+})()
